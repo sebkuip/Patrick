527cb4d16fa52437eda7dc46dc6b7d6709d7f456
Sebastian Kuipers <kuipers.sebastian@ziggo.nl>
2025-03-21 12:10:36 +0100

    clean up xkcd command

--- cogs/funcommands.py
+++ cogs/funcommands.py
@@ -22,6 +22,24 @@ class FunCommands(commands.Cog):
                 data = await response.json()
                 await ctx.send(f"\"{data[0]['q']}\" - {data[0]['a']}")
 
+    async def get_xkcd_data(self, number: int):
+        async with self.bot.aiosession.get(f"https://xkcd.com/{number}/info.0.json") as response:
+            if response.status == 200:
+                data = await response.json()
+                return data
+            else:
+                return None
+
+    def xkcd_embed(self, data):
+        embed = discord.Embed(
+            title=data["title"],
+            description=data["alt"],
+            url=f"https://xkcd.com/{data['num']}",
+            image=data["img"],
+        )
+        embed.set_footer(text=f"{data['num']} ({data['month']}/{data['day']}/{data['year']})")
+        return embed
+
     @commands.command()
     async def xkcd(self, ctx, number: int = None):
         if number is None:
@@ -29,30 +47,16 @@ class FunCommands(commands.Cog):
                 if response.status == 200:
                     data = await response.json()
                     randnum = randint(1, data["num"])
-                    async with self.bot.aiosession.get(f"https://xkcd.com/{randnum}/info.0.json") as response:
-                        if response.status == 200:
-                            data = await response.json()
-                            embed = discord.Embed(
-                                title=data["title"],
-                                description=data["alt"],
-                                url=f"https://xkcd.com/{randnum}",
-                                image=data["img"],
-                            )
-                            embed.set_footer(text=f"{randnum} ({data['month']}/{data['day']}/{data['year']})")
-                            await ctx.send(embed=embed)
+                    data = await self.get_xkcd_data(randnum)
+                    if data is None:
+                        return await ctx.send("An error occurred while fetching the comic.")
+                    await ctx.send(embed=self.xkcd_embed(data))
 
         else:
-            async with self.bot.aiosession.get(f"https://xkcd.com/{number}/info.0.json") as response:
-                if response.status == 200:
-                    data = await response.json()
-                    embed = discord.Embed(
-                        title=data["title"],
-                        description=data["alt"],
-                        url=f"https://xkcd.com/{number}",
-                        image=data["img"],
-                    )
-                    embed.set_footer(text=f"{number} ({data['month']}/{data['day']}/{data['year']})")
-                    await ctx.send(embed=embed)
+            data = await self.get_xkcd_data(number)
+            if data is None:
+                return await ctx.send("An error occurred while fetching the comic.")
+            await ctx.send(embed=self.xkcd_embed(data))
 
 def setup(bot):
     bot.add_cog(FunCommands(bot))
