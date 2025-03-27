633af09cdc8c7396f5d3e31f32c0bbaa9ba3361a
Sebastian Kuipers <kuipers.sebastian@ziggo.nl>
2025-03-27 12:06:03 +0100

    is_staff and is_admin now properly error

--- util.py
+++ util.py
@@ -33,19 +33,17 @@ async def process_custom_command(bot, message) -> bool:
 
 def is_staff():
     def predicate(ctx):
-        return (
-            discord.utils.get(ctx.author.roles, id=ctx.bot.config["roles"]["staff"])
-            is not None
-        )
-
+        if discord.utils.get(ctx.author.roles, id=ctx.bot.config["roles"]["staff"]) is not None:
+            return True
+        else:
+            raise commands.MissingPermissions("You are not staff.")
     return commands.check(predicate)
 
 
 def is_admin():
     def predicate(ctx):
-        return (
-            discord.utils.get(ctx.author.roles, id=ctx.bot.config["staff"]["admin"])
-            is not None
-        )
-
+        if discord.utils.get(ctx.author.roles, id=ctx.bot.config["roles"]["admin"]) is not None:
+            return True
+        else:
+            raise commands.MissingPermissions("You are not an admin.")
     return commands.check(predicate)
