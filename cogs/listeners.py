0375d5846c42ae71f6b756850b9f74dbae841346
sebkuip <kuipers.sebastian@ziggo.nl>
2025-08-27 20:20:21 +0200

    Move the on_thread_create listener

--- cogs/listeners.py
+++ cogs/listeners.py
@@ -12,5 +12,12 @@ class Listeners(commands.Cog):
         channel: discord.abc.Messageable = member.guild.get_channel(self.bot.config["channels"]["welcome"])
         await channel.send(greeting.format(user=member.mention))
 
+    @commands.Cog.listener()
+    async def on_thread_create(self, thread: discord.Thread):
+        if thread.parent_id in self.bot.config["autosub_forums"]:
+            role = await thread.guild.fetch_role(self.bot.config["roles"]["staff"])
+            for member in role.members:
+                await thread.add_user(member)
+
 async def setup(bot: commands.Bot):
     await bot.add_cog(Listeners(bot))
