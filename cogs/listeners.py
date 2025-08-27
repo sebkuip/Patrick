b63af13b7173fbcde359fdc72f8b4cc446a961c5
sebkuip <kuipers.sebastian@ziggo.nl>
2025-08-27 20:19:31 +0200

    You can't delete thread add messages :(

--- cogs/listeners.py
+++ cogs/listeners.py
@@ -7,11 +7,6 @@ class Listeners(commands.Cog):
         self.bot = bot
 
     @commands.Cog.listener()
-    async def on_message(self, message: discord.Message):
-        if message.type == discord.MessageType.recipient_add and message.author == self.bot.user:
-            await message.delete()
-
-    @commands.Cog.listener()
     async def on_member_join(self, member: discord.Member):
         greeting: str = choice(self.bot.config["greetings"])
         channel: discord.abc.Messageable = member.guild.get_channel(self.bot.config["channels"]["welcome"])
